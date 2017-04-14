75fe05110ecb6c1b66b0a138a996ec2d08c0a16a
bthaman <william.thaman@gmail.com>
2017-04-14 16:19:24 -0500

    initial commit

--- setup_exe.py
+++ setup_exe.py
@@ -2,4 +2,12 @@ from distutils.core import setup
 import py2exe
 
 
-setup(windows=['pws_app.py'], options={'py2exe': {'includes': ['lxml.etree', 'lxml._elementpath', 'gzip']}})
+option = 'console'
+
+if option == 'windows':
+    # in windows option, command window is not displayed
+    setup(windows=['pws_app.py'], options={'py2exe': {'includes': ['lxml.etree', 'lxml._elementpath', 'gzip']}})
+else:
+    # in console option, command window is displayed
+    setup(console=['pws_app.py'], options={'py2exe': {'includes': ['lxml.etree', 'lxml._elementpath', 'gzip']}})
+
